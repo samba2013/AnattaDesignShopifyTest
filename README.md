8f4cb4faf5aec14af636144eae00a7da5a60006e
Mimoun EL-LEBBAR <mimoun@thexroadz.com>
2025-01-10 11:16:30 -0500

    ascending variants by price

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # AnattaDesignShopifyTest
 Anatta Design Shopify Test
+
+Step for test:
+
+1 - create .env file with two params filled with credentials:
+
+SHOPIFY_ADMIN_SHOP=
+SHOPIFY_ADMIN_ACCESS_TOKEN=
+
+2 - install node modules:
+    npm install
+
+3 - run test:
+
+    node app.js --name shirt
+
+
+e.g output :
+
+Output:
+A shirt - 3 / black - price $15.00
+A shirt - 3 / white - price $15.00
+A shirt - 3 / red - price $15.00
+A shirt - 3 / blue - price $15.00
+A shirt - 2 / black - price $25.00
+A shirt - 2 / white - price $25.00
+A shirt - 2 / red - price $25.00
+A shirt - 2 / blue - price $25.00
+A shirt - 5 / black - price $30.00
+A shirt - 5 / white - price $30.00
+A shirt - 5 / red - price $30.00
+A shirt - 5 / blue - price $30.00
+A shirt - 4 / black - price $35.00
+A shirt - 4 / white - price $35.00
+A shirt - 4 / red - price $35.00
+A shirt - 4 / blue - price $35.00
--- app.js
+++ app.js
@@ -3,8 +3,6 @@ dotenv.config();
 import fetch from 'node-fetch';
 const { SHOPIFY_ADMIN_SHOP, SHOPIFY_ADMIN_ACCESS_TOKEN } = process.env;
 import { parseArgs } from 'node:util';
-import https from 'https';
-
 
 // Configure input args
 const options = {
@@ -17,65 +15,148 @@ const options = {
   
   const { values, positionals } = parseArgs({ options });
   
-const productSearchName = values.name; // product name input search
-  
+
 
 // Method 1: Using Shopify API directly using Fetch
-
-const shopifyAdminGqlEndpoint = `https://${SHOPIFY_ADMIN_SHOP}.myshopify.com/admin/api/2025-01/graphql.json`;
-
+const shopifyAdminGqlEndpoint = `https://${SHOPIFY_ADMIN_SHOP}/admin/api/2025-01/graphql.json`;
 
 
-const runQueryWithPagination = (query,variables)=>{
+// ########################## FUNCTIONS REGION
+const runQueryWithPagination = async(query,variables)=>{
+  let payload = {
+    query: query,
+    variables: variables
+  };
 
-    // Make a GraphQL request to Shopify
-    fetch(shopifyAdminGqlEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': SHOPIFY_ADMIN_ACCESS_TOKEN,
-    },
-    body: JSON.stringify({ gqlQuery }),
-    agent: httpsAgent
-  })
-    .then(response => response.json())
-    .then(data => {
-      return data;
-    })
-    .catch(error => console.error('Error fetching products:', error));
+  let hasNextPage = true;
+  let data = {};
+  let result = null;
+  let cursor = null;
+  let maxRetries = 5;
+
+  while(hasNextPage && maxRetries >0){
+      
+    if(cursor){
+      payload.variables.endCursor = cursor;
+    }
+      result = await fetch(shopifyAdminGqlEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token': SHOPIFY_ADMIN_ACCESS_TOKEN,
+          },
+          body: JSON.stringify(payload),
+        })
+          .then(response => response.json())
+          .catch(error => {
+            console.error('Error fetching products:', error)
+            return null;
+          });
+
+ 
+      if(result === null) break;
+      hasNextPage = result.data.products.pageInfo.hasNextPage;
+      if(cursor === result.data.products.pageInfo.endCursor){
+        maxRetries--; // prevent infinite loop
+        continue;
+      }
+
+
+      cursor = result.data.products.pageInfo.endCursor;
+      result.data.products.edges.forEach((edge,index)=>{
+          data[edge.node.id]={
+            id:edge.node.id,
+            title: edge.node.title,
+            variants: edge.node.variants.nodes
+          }
+      });
+      
+      await sleep(1500); // sleep 1.5 seconds before next page
+  }
+  return data;
 
 }
 
 
-const getProductsByName = (productName)=>{
-    console.log(`Looking for products with name : ${productName}`);
-    // Getting Products by input variable : name
-    const gqlQuery = `
-      {
-        products(first: 10, query: "title: '${productName}'") {
-          pageInfo{
-            endCursor
-            hasNextPage
-          }
-          edges {
-            cursor
-            node {
-              id
-              title
-              variantsCount
+const getProductsByName = async(productName)=>{
+  // Getting Products by input variable : name
+  const gqlQuery = `
+    query ($productSearchQuery: String!, $endCursor: String){
+      products(first: 10, query: $productSearchQuery, after: $endCursor) {
+        pageInfo{
+          endCursor
+          hasNextPage
+        }
+        edges {
+          cursor
+          node {
+            id
+            title
+            variants(first: 100){
+              nodes{
+                id
+                title
+                price
+              }
             }
           }
         }
       }
-    `;
+    }
+  `;
 
-    const httpsAgent = new https.Agent({
-        rejectUnauthorized: false,
+  const products = await runQueryWithPagination(gqlQuery,
+      {
+      'productSearchQuery':`title:${productName}*`
+      }
+    );
+  // sort by variant price :
+  // because variants connection doesn't have sortKey for price sorting, i'll do it manually
+  const productIds  = Object.keys(products);
+  if(productIds.length){
+    const flattenVariants = [];
+    productIds.forEach((gid,index)=>{
+      products[gid].variants.forEach((variantNode,index)=>{
+        flattenVariants.push(
+          {
+            variantTitle: variantNode.title,
+            price: variantNode.price,
+            productTitle: products[gid].title
+          }
+        )
       });
-    
-    
-};
+    });
+    // Sort by price (ascending)
+    flattenVariants.sort((a, b) => a.price - b.price);
 
+    console.log("Output:");
+    flattenVariants.forEach(data => {
+      console.log(`${data.productTitle} - ${data.variantTitle} - price $${data.price}`);
+    });
+  }else{
+    console.log(`Oops! no products found`);
+  }
 
   
+  
+};
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+  
+// ######################### END FUNCTION REGION
+
+
+
+// ######################### Main execution
+const productSearchName = values.name; // product name input search
+if(productSearchName == undefined)  {
+  console.log(`Product name to search for is required!`);
+  process.exit();
+}
+getProductsByName(productSearchName);
+
+
+
 
